090b3c380fdd9547c34cab11728cae00b6aad67c
Alastair Carr <AliCarr@users.noreply.github.com>
2020-04-10 21:15:42 +0100

    Continuation of hoverboard
    The board now has the proper(ish) logic for the hover aspect. Some testing has been done on possible additions, still figuring out what to do with this

---
+++ Mechanics Arena/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public GameObject followTarget;
+    Vector3 difference;
+    // Start is called before the first frame update
+    void Start()
+    {
+        difference = transform.position - followTarget.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        difference = Quaternion.AngleAxis(Input.GetAxis("Mouse X") * 1, Vector3.up) * difference;
+        transform.SetPositionAndRotation(followTarget.transform.position + difference, transform.rotation);
+        transform.LookAt(followTarget.transform);
+        
+    }
+}
--- Mechanics Arena/Assets/Scripts/Hoverboard.cs
+++ Mechanics Arena/Assets/Scripts/Hoverboard.cs
@@ -7,50 +7,65 @@ using UnityEngine;
 
 public class Hoverboard : MonoBehaviour
 {
+    public Transform[] trans = new Transform[4];
+    public bool isThisThePlayerHoverCraft;
+
     Vector3 [] edgePoints = new Vector3[4];
     Vector3 [] hitPoints = new Vector3[4];
-    public Vector3 forces= Vector3.zero;
-    public Vector3 diff = Vector3.zero;
-    public Rigidbody rigidbody;
-    // Start is called before the first frame update
-    void Start()
+    Vector3 forces= Vector3.zero;
+    Vector3 diff = Vector3.zero;
+    Rigidbody myRigidBody;
+    
+
+    private void Start()
     {
-        //rigidbody = GetComponent<Rigidbody>();
-        hitPoints[0] = new Vector3(transform.position.x - 1, transform.position.y, transform.position.z + 2); //Front left
-        hitPoints[1] = new Vector3(transform.position.x + 1, transform.position.y, transform.position.z + 2); //Front right
-        hitPoints[2] = new Vector3(transform.position.x - 1, transform.position.y, transform.position.z - 2); //Back left
-        hitPoints[3] = new Vector3(transform.position.x + 1, transform.position.y, transform.position.z - 2); //Back right
+        myRigidBody = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         UpdateTheEdgePoints();
         CastRays();
+        SlantCheck();
+
+        if(isThisThePlayerHoverCraft)
+            HandleInputs();
 
         for(int c = 0; c < 4; c++)
         {
-            diff = edgePoints[c] - hitPoints[c];
-            if (diff.y < 3)
-                forces = new Vector3(0, 240 - diff.y * 100, 0);
-            else
-                forces = Vector3.zero;
-
-            //forces = new Vector3(0, 10, 0);
-
-            rigidbody.AddForceAtPosition(forces, edgePoints[c]);
+            diff = (edgePoints[c] - hitPoints[c])*1.3f;
+            forces = new Vector3(0, (Mathf.Pow(0.78f, ((diff.y*2) - 1.7f)) - 0.2f)* 600, 0);
+            myRigidBody.AddForceAtPosition(forces, edgePoints[c]);
         }
 
-        //Vector3 diff = hitPoints[0] - edgePoints[0];
-        //rigidbody.AddForceAtPosition(diff.normalized, edgePoints[0]);
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            myRigidBody.AddForce(transform.forward * 50000);
+        }
+
+        //if (Input.GetKeyDown(KeyCode.Space))
+        //{
+        //    int ax = Random.Range(0, 4);
+        //    myRigidBody.AddForceAtPosition(new Vector3(0, 1200, 0), edgePoints[ax]);
+        //}
+    }
+
+    void HandleInputs()
+    {
+        if(Input.GetKey(KeyCode.W))
+            myRigidBody.AddForce(transform.forward * 1200);
+
+        if (Input.GetKey(KeyCode.A))
+            myRigidBody.AddTorque(Vector3.up * -230);
+
+        if (Input.GetKey(KeyCode.D))
+            myRigidBody.AddTorque(Vector3.up * 230);
     }
 
     void UpdateTheEdgePoints()
     {
-        edgePoints[0] = new Vector3(transform.position.x - 1, transform.position.y, transform.position.z + 2); //Front left
-        edgePoints[1] = new Vector3(transform.position.x + 1, transform.position.y, transform.position.z + 2); //Front right
-        edgePoints[2] = new Vector3(transform.position.x - 1, transform.position.y, transform.position.z - 2); //Back left
-        edgePoints[3] = new Vector3(transform.position.x + 1, transform.position.y, transform.position.z - 2); //Back right
+        for(int c = 0; c < 4; ++c)
+            edgePoints[c] = trans[c].position; //Front left
     }
 
     void CastRays()
@@ -60,13 +75,32 @@ public class Hoverboard : MonoBehaviour
         layerMask = ~layerMask;
 
         for (int c = 0; c < 4; ++c)
-        {
-            if (Physics.Raycast(edgePoints[c], new Vector3(0, -1, 0), out hit, Mathf.Infinity, layerMask))
-            {
-                Debug.DrawRay(edgePoints[c], -Vector3.up * hit.distance, Color.red);
+            if (Physics.Raycast(edgePoints[c], new Vector3(0, -1, 0), out hit, Mathf.Infinity, layerMask))             
                 hitPoints[c] = new Vector3(edgePoints[c].x, edgePoints[c].y - hit.distance, edgePoints[c].z);
-            }
-        }
+
+    }
+
+    void SlantCheck()
+    {
+        //get the current angle relative to the X axis. If it is greater than a value, add a small amount of force in that direction
+        float myAngleX = WrapAngle(transform.eulerAngles.x);
+        float myAngleZ = WrapAngle(transform.eulerAngles.z);
+
+        if (myAngleX >= 20 || myAngleX <= -20)
+            myRigidBody.AddForce(transform.forward * 15 * myAngleX);
+
+        if (myAngleZ >= 20 || myAngleZ <= -20)
+            myRigidBody.AddForce(-transform.right * 15 * myAngleZ);
+    }
+
+    private float WrapAngle(float angle)
+    {
+        angle %= 360;
+
+        if (angle > 180)
+            return angle - 360;
+
+        return angle;
     }
 
     private void OnDrawGizmos()
@@ -76,6 +110,20 @@ public class Hoverboard : MonoBehaviour
         {
             Gizmos.DrawSphere(edgePoints[c], 0.3f);
             Gizmos.DrawSphere(hitPoints[c], 0.3f);
+            Debug.DrawRay(edgePoints[c], Vector3.up * (hitPoints[c].y - edgePoints[c].y), Color.red);
         }
     }
 }
+
+
+/*
+ BUG LIST:
+    If it flips, the logic for sliding fucks up. 
+
+
+FEATURE LIST:
+    
+     
+     
+     
+     */
